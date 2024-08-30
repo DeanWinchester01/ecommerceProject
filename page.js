923712a9332f919c8adde60fed93991accf1a5f4
DeanWinchester46 <59614870+worldkiIIer@users.noreply.github.com>
2024-08-30 20:01:42 +1200

    fixed main page javascript

---
+++ page.js
@@ -0,0 +1,78 @@
+var items = [
+    {
+        "name":"suzuki",
+        "category":"bike",
+        "description":"some description",
+        "price":6000
+    },
+    {
+        "name":"jefferson",
+        "category":"bike",
+        "description":"i dont know",
+        "price":7000
+    },
+    {
+        "name":"third bike",
+        "category":"i dont know, im tired",
+        "description":"jez, dickhead, do this for me",
+        "price":50_000
+    }
+]
+for(let i = 0; i < 12; i++){
+    var layout = document.getElementById("layout")
+    var newButton = document.createElement("button")
+    newButton.className = "item"
+    newButton.id = i
+    layout.appendChild(newButton)
+
+    var image = document.createElement("img")
+    var source = i+".png"
+    image.src = source
+    newButton.appendChild(image)
+
+    image.className = "itemImage"
+
+    newButton.onclick = function(){
+        ShowItem(i)
+    }
+}
+
+var itemView = document.getElementById("itemview")
+var sidemenu = document.getElementById("sidemenu")
+var layout = document.getElementById("layout")
+var searchBar = document.getElementById("searchbar")
+
+var itemName = document.getElementById("name")
+var category = document.getElementById("category")
+var desc = document.getElementById("description")
+var price = document.getElementById("price")
+
+var closeButton = document.getElementById("close")
+
+function ShowBackground(){
+    itemView.style.visibility = "hidden"
+    sidemenu.style.filter = "none"
+    layout.style.filter = "none"
+    searchBar.style.filter = "none"
+}
+
+async function ShowItem(index){
+    let entry = items[index]
+    let name = itemName.querySelector("#custom")
+    let cat = category.querySelector("#custom")
+    let description = desc.querySelector("#custom")
+    let pr = price.querySelector("#custom")
+    let img = document.getElementById("itemImage")
+
+    name.textContent = entry.name
+    cat.textContent = entry.category
+    description.textContent = entry.description
+    pr.textContent = "$"+entry.price
+    itemView.style.visibility = "visible"
+    img.src = index+".png"
+}
+
+closeButton.onclick = ShowBackground
+//document.getElementById("0").onclick = function(){
+    //ShowItem("0")
+//}
