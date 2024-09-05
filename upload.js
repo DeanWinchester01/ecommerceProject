414fdbec0fd35ae919bde39fa7657485ba4417e2
Dean Winchester <59614870+worldkiIIer@users.noreply.github.com>
2024-09-05 14:26:55 +1200

    made buttons in upload page work

---
+++ upload.js
@@ -0,0 +1,55 @@
+var uploadButton = document.getElementById("imgUpload")
+var vehicleImg = document.getElementById("itemImg")
+var upload = document.getElementById("Upload")
+
+
+
+console.log(uploadButton)
+uploadButton.onchange = function(){
+    console.log("got file")
+    let file = uploadButton.files[0]
+    let reader = new FileReader()
+    reader.onload = function(e){
+        vehicleImg.src = e.target.result
+    }
+    reader.readAsDataURL(file)
+    //vehicleImg.src = file.name
+    console.log(file)
+}
+
+upload.onclick = function(){
+    //input fields
+    let itemName = document.getElementById("itemName")
+    let desc = document.getElementById("desc")
+    let price = document.getElementById("price")
+    let category = document.getElementById("category")
+    console.log(itemName)
+    let canUpload = true
+
+    if(itemName.value == ""){
+        itemName.value = "Fill in this field"
+        canUpload = false
+    }
+
+    if(desc.value == ""){
+        desc.value = "Fill in this field"
+        canUpload = false
+    }
+
+    if(price.value == ""){
+        price.value = "Fill in this field"
+        canUpload = false
+    }
+
+    if(category.value == ""){
+        category.value = "Fill in this field"
+        canUpload = false
+    }
+
+    if(!canUpload){
+        return
+    }
+    
+    console.log("Upload")
+    window.open("page.html")
+}
